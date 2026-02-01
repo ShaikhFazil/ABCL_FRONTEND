258ab976afd7043b1f9ede9e76f9678ffa9724b6
ShaikhFazil <shaikhfazil180@gmail.com>
2026-02-01 18:04:49 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 import { fileURLToPath } from "url";
 
+// ESM-safe __dirname replacement
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -10,7 +11,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
